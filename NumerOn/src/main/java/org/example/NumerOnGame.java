b94cf6139625c54bc475e05b14356703e2eabd9b
Noe0131 <KTC2A31G0017@edu.kyoto-tech.ac.jp>
2025-11-20 09:15:09 +0900

    ヌメロンユーザー側配列代入完了

--- NumerOn/src/main/java/org/example/NumerOnGame.java
+++ NumerOn/src/main/java/org/example/NumerOnGame.java
@@ -1,17 +1,21 @@
 package org.example;
 
+import java.sql.SQLOutput;
+import java.util.Arrays;
 import java.util.Random;
+import java.util.Scanner;
 
 public class NumerOnGame {
     int _maxTry;
     int[] answer = new int[3];
+    Scanner scan = new Scanner(System.in);
 
     public NumerOnGame(int _maxTry) {
         this._maxTry = _maxTry;
     }
 
     public void createAnswer() {
-        Random num = new Random();
+        Random random = new Random();
 
         for (int i = 0; i < answer.length; i++) {
             int number;
@@ -19,9 +23,9 @@ public class NumerOnGame {
             while (true) {
 
                 if (i == 0) {
-                    number = num.nextInt(9) + 1;
+                    number = random.nextInt(9) + 1;
                 } else {
-                    number = num.nextInt(10);
+                    number = random.nextInt(10);
                 }
 
                 boolean duplicate = false;
@@ -43,12 +47,37 @@ public class NumerOnGame {
         }
     }
 
+    void checkEatBite(int[] input_user) {
+
+    }
+
+    public void splitNumber() {
+
+        int input = scan.nextInt();
+
+        int Hundreds_place = input / 100;
+        int Tens_place = (input / 10) % 10;
+        int Ones_place = input % 10;
+
+        int[]  input_user;
+        input_user  = new int[3];
+
+        input_user[0] = Hundreds_place;
+        input_user[1] = Tens_place;
+        input_user[2] = Ones_place;
+
+        System.out.println(Arrays.toString(input_user));
+
+    }
+
     void start() {
         createAnswer();
+        splitNumber();
         System.out.print("答え: ");
         for (int n : answer) {
             System.out.print(n + "");
         }
         System.out.println();
+        scan.close();
     }
 }
